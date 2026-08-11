44ef5118504f34ccdd9c6c6328b807564e19894f
Kai Pearson <kai.pearson@dfo-mpo.gc.ca>
2026-08-11 13:44:56 -0400

    Added transform header file structure

---
+++ include/Transform.hpp
@@ -0,0 +1,30 @@
+#ifndef TRANSFORM_HPP
+#define TRANSFORM_HPP
+
+#include "Vec3.hpp"
+
+namespace hybriddisplay::math {
+
+class Transform {
+private:
+    Vec3 position;
+    Vec3 rotation;
+    Vec3 scale;
+public:
+    
+    Transform(const Vec3& position = Vec3(0, 0, 0), const Vec3& rotation = Vec3(0, 0, 0), const Vec3& scale = Vec3(1, 1, 1));
+
+    void setPosition(const Vec3& position);
+    void setRotation(const Vec3& rotation);
+    void setScale(const Vec3& scale);
+
+    const Vec3 getPosition() const;
+    const Vec3 getRotation() const;
+    const Vec3 getScale() const;
+
+    Vertex apply(const Vertex& vertex) const; // rotation and scale are applied first, then position is applied to the vertex -> make sure your mesh is centered
+};
+
+};
+
+#endif
